07c30f14976bb58e3c1c13c0d41ea638350cbb99
Akwasi Nyarko <nyarkokyei14@gmail.com>
2025-11-09 00:41:19 -0600

    Solved valid parentheses leetcode question

---
+++ ValidParentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        mapping = {
+            "}" : "{",
+            "]" : "[",
+            ")" : "("
+        }
+
+        for ch in s:
+            if ch in mapping.keys():
+                stack.append(ch)
+            else:
+                if stack:
+                    if mapping[ch] == stack[-1]:
+                        stack.pop()
+                    else:
+                        return False
+                else:
+                    return False
+        return len(stack) == 0
